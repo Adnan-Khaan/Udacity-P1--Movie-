574eda3f59a6c8bfff45907adc58bdeb6dea47f2
Adnan-Khaan <muhd.adnan@gmail.com>
2015-06-05 14:28:55 -0700

    Create movie.py

---
+++ movie.py
@@ -0,0 +1,12 @@
+import webbrowser
+#defining the class Movie and constructor. 
+class Movie(): 
+     def __init__(self,movie_title,movie_storyline,poster_image,trailer_youtube,rating,actors,release):
+        self.title = movie_title
+        self.storyline = movie_storyline
+        self.poster_image_url = poster_image
+        self.trailer_youtube_url = trailer_youtube
+        self.rating = rating
+        self.release= release
+        self.actors = actors  
+
