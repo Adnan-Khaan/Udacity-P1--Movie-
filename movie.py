6512a6d5b3c5df1915650c393cf1286fb8de1bc5
Adnan-Khaan <muhd.adnan@gmail.com>
2015-06-08 17:10:30 -0700

    Update movie.py

--- movie.py
+++ movie.py
@@ -1,12 +1,31 @@
 import webbrowser
-#defining the class Movie and constructor. 
-class Movie(): 
-     def __init__(self,movie_title,movie_storyline,poster_image,trailer_youtube,rating,actors,release):
+
+
+class Movie():
+    '''
+    This class contains all the attibutes that are 
+    required to save information about a movie.
+    '''
+
+    def __init__(
+        self,
+        movie_title,
+        movie_storyline,
+        poster_image,
+        trailer_youtube,
+        rating,
+        actors,
+        release,
+                  ):
+        '''
+        This is the definition of the contructor of class Movie, which
+        will be called whenever an object is being created.
+        '''    
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
         self.rating = rating
-        self.release= release
-        self.actors = actors  
-
+        self.release = release
+        self.actors = actors     
+    
